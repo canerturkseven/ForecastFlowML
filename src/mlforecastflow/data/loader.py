ed4609d13950d4fd926e875486a9643fd0daecb9
Caner Turkseven <canerturkseven@gmail.com>
2023-01-25 02:16:19 +0100

    fixed walmart data loader

--- src/mlforecastflow/data/loader.py
+++ src/mlforecastflow/data/loader.py
@@ -1,8 +1,12 @@
+import os
 import pyspark.sql.functions as F
 
-def load_walmart():
-    df = spark.read.parquet("./walmart")
+
+def load_walmart(spark):
+    path = os.path.join(os.path.dirname(__file__), "walmart")
+    df = spark.read.parquet(path).withColumn("date", F.to_timestamp("date"))
     df_train = df.filter(F.col("date") < "2016-01-01")
     df_test = df.filter(F.col("date") >= "2016-01-01")
     return df_train, df_test
+
 
