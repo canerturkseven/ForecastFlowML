58a25002d785d4fcc7b818141b39481187869a05
Caner Turkseven <canerturkseven@gmail.com>
2023-01-25 01:08:18 +0100

    fix walmart typo

--- src/mlforecastflow/data/loader.py
+++ src/mlforecastflow/data/loader.py
@@ -1,7 +1,7 @@
 import pyspark.sql.functions as F
 
-def load_wallmart():
-    df = spark.read.parquet("./wallmart")
+def load_walmart():
+    df = spark.read.parquet("./walmart")
     df_train = df.filter(F.col("date") < "2016-01-01")
     df_test = df.filter(F.col("date") >= "2016-01-01")
     return df_train, df_test
