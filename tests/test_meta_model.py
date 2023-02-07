5af0493b8c146f6118d65c83d60e6196352bd6b4
canerturkseven <canerturkseven@gmail.com>
2023-02-07 03:28:59 +0100

    test spark creation

--- tests/test_meta_model.py
+++ tests/test_meta_model.py
@@ -1,2 +1,8 @@
+from pyspark.sql import SparkSession
+
 def test_dummy():
+    assert 1 == 1
+
+def test_spark_create():
+    spark = SparkSession.builder.master("local[*]").getOrCreate()
     assert 1 == 1
