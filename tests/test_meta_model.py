20e7b969139dd6bb430328d7550aaff2c5142350
canerturkseven <canerturkseven@gmail.com>
2023-02-07 03:32:38 +0100

    test spark

--- tests/test_meta_model.py
+++ tests/test_meta_model.py
@@ -1,8 +1,10 @@
 from pyspark.sql import SparkSession
 
+
 def test_dummy():
     assert 1 == 1
 
+
 def test_spark_create():
     spark = SparkSession.builder.master("local[*]").getOrCreate()
-    assert 1 == 1
+    spark.range(5).show()
