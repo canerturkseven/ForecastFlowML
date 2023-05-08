15261f701762166e935d4c38b609a394163c6e25
canerturkseven <canerturkseven@gmail.com>
2023-05-08 03:45:29 +0200

    try spark <3

--- tests/test_pyspark.py
+++ tests/test_pyspark.py
@@ -1,3 +1,7 @@
+import pyspark
+import pyspark.sql.functions as F
+
+
 def test_pyspark(spark):
     spark.range(10).show()
     assert True
@@ -9,6 +13,15 @@ def test_pandas_udf(spark):
     def udf(df_pandas):
         return df_pandas
 
-    assert (
-        df.groupby("id").applyInPandas(udf, schema=df.schema).collect() == df.collect()
-    )
+    if pyspark.__version__ < "3":
+        pandas_udf = F.pandas_udf(
+            udf,
+            returnType=df.schema,
+            functionType=F.PandasUDFType.GROUPED_MAP,
+        )
+        assert df.groupby("id").apply(pandas_udf).collect() == df.collect()
+    else:
+        assert (
+            df.groupby("id").applyInPandas(udf, schema=df.schema).collect()
+            == df.collect()
+        )
