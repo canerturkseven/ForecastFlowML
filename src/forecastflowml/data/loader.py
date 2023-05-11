4831c0755cf799ea681122649abe44b948770219
canerturkseven <canerturkseven@gmail.com>
2023-05-11 02:01:47 +0200

    reduce sample dataset size

--- src/forecastflowml/data/loader.py
+++ src/forecastflowml/data/loader.py
@@ -1,7 +1,5 @@
 import os
-import pandas as pd
-
 
 def load_walmart_m5(spark):
     path = os.path.join(os.path.dirname(__file__), "walmart_m5")
-    return spark.createDataFrame(pd.read_parquet(path))
+    return spark.read.parquet(path)
