7011d60e08f6a24fcd6200d095d10fd6f8607937
canerturkseven <canerturkseven@gmail.com>
2023-05-11 23:51:06 +0200

    fix PYSPARK_PYTHON path

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from pyspark.sql import SparkSession
 
 @pytest.fixture(scope="session")
 def spark():
+    os.environ["PYSPARK_PYTHON"] = sys.executable
 
     spark = (
         SparkSession.builder.master("local[1]")
